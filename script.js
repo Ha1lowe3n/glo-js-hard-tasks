cd9efbe8b8e0144b2c968fa9465f94a6e4ecdf39
Igor Trifonov <Ha1lowe3n@yandex.ru>
2020-10-10 07:10:56 +0800

    fix(script.js): made the arrow function in one line

--- script.js
+++ script.js
@@ -1,13 +1,12 @@
 let num = 266219;
 
-let result = num.toString().split('').reduce( (a, b) => {
-    return a * b;
-});
+let result = num.toString().split('').reduce( (a, b) => a * b);
 console.log(result);
 
 let degree = result ** 3;
 console.log(degree);
 
 alert(degree.toString().slice(0, 2));
+
 
 
