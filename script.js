a8d46a69a236dcd1393b841263d6d9134e7eb2dd
Igor Trifonov <Ha1lowe3n@yandex.ru>
2020-10-12 21:58:59 +0800

    feat(script.js): new feat

--- script.js
+++ script.js
@@ -1,12 +1,55 @@
-let num = 266219;
+'use strict';
 
-let result = num.toString().split('').reduce( (a, b) => a * b);
+let lang = 'ru';
+
+const stateLangDays = {
+  ru: ['Понедельник', 'Вторник', 'Среда', 'Четверг', 'Пятница', 'Суббота', 'Воскресенье'],
+  en: ['Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday', 'Sunday']
+};
+
+// 1
+if (lang === 'ru') {
+  console.log(stateLangDays.ru);
+} else if (lang === 'en') {
+  console.log(stateLangDays.en);
+} else {
+  console.log('Произошла ошибка');
+}
+
+switch (lang) {
+  case 'ru':
+    console.log(stateLangDays.ru);
+    break;
+  case 'en':
+    console.log(stateLangDays.en);
+    break;
+  default:
+    console.log('Произошла ошибка');
+}
+
+let langArr = [];
+langArr.ru = stateLangDays.ru;
+langArr.en = stateLangDays.en;
+console.log(langArr[lang]);
+
+// 2
+let namePerson = 'Артём';
+
+let result = (namePerson === 'Артём') ? 'директор' :
+  (namePerson === 'Максим') ? 'преподаватель' :
+  'студент';
 console.log(result);
 
-let degree = result ** 3;
-console.log(degree);
 
-alert(degree.toString().slice(0, 2));
+
+
+
+
+
+
+
+
+
 
 
 
