1ff4d2a0f93bd8e989b64f6e88c8131a0f11a395
Igor Trifonov <Ha1lowe3n@yandex.ru>
2020-10-13 09:05:32 +0800

    fix(script.js): fix all

--- script.js
+++ script.js
@@ -1,6 +1,8 @@
 'use strict';
 
-let lang = 'ru';
+let lang = prompt('Выберите язык: ru или en');
+console.log(typeof(lang));
+console.log(lang);
 
 const stateLangDays = {
   ru: ['Понедельник', 'Вторник', 'Среда', 'Четверг', 'Пятница', 'Суббота', 'Воскресенье'],
@@ -8,29 +10,31 @@ const stateLangDays = {
 };
 
 // 1
-if (lang === 'ru') {
-  console.log(stateLangDays.ru);
-} else if (lang === 'en') {
-  console.log(stateLangDays.en);
+if (lang !== 'ru' && lang !== 'en') {
+  console.log('Язык может быть только ru или en');
 } else {
-  console.log('Произошла ошибка');
-}
-
-switch (lang) {
-  case 'ru':
+  if (lang === 'ru') {
     console.log(stateLangDays.ru);
-    break;
-  case 'en':
+  } else if (lang === 'en') {
     console.log(stateLangDays.en);
-    break;
-  default:
+  } else {
     console.log('Произошла ошибка');
+  }
+  
+  switch (lang) {
+    case 'ru':
+      console.log(stateLangDays.ru);
+      break;
+    case 'en':
+      console.log(stateLangDays.en);
+      break;
+    default:
+      console.log('Произошла ошибка');
+  }
+  
+  console.log(stateLangDays[lang]);  
 }
 
-let langArr = [];
-langArr.ru = stateLangDays.ru;
-langArr.en = stateLangDays.en;
-console.log(langArr[lang]);
 
 // 2
 let namePerson = 'Артём';
