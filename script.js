8e2cf974318fd6ddfefd0793bf8de46af5be78c0
Igor Trifonov <Ha1lowe3n@yandex.ru>
2020-10-13 23:55:02 +0800

    feat(script.js): new feat

--- script.js
+++ script.js
@@ -1,46 +1,23 @@
 'use strict';
 
-let lang = prompt('Выберите язык: ru или en');
-
-const stateLangDays = {
-  ru: ['Понедельник', 'Вторник', 'Среда', 'Четверг', 'Пятница', 'Суббота', 'Воскресенье'],
-  en: ['Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday', 'Sunday']
-};
-
-// 1
-if (lang === 'ru' || lang === 'en') {
-  if (lang === 'ru') {
-    console.log(stateLangDays.ru);
-  } else if (lang === 'en') {
-    console.log(stateLangDays.en);
+const deleteSpaces = (str) => {
+  let newStr;
+  if (typeof str === 'string') {
+    newStr = str.trim();
+    if (newStr.length > 30) {
+      newStr = newStr.substr(0, 30) + '...';
+    }
   } else {
-    console.log('Произошла ошибка');
+    newStr = alert('Вы не передали строку');
   }
-  
-  switch (lang) {
-    case 'ru':
-      console.log(stateLangDays.ru);
-      break;
-    case 'en':
-      console.log(stateLangDays.en);
-      break;
-    default:
-      console.log('Произошла ошибка');
-  }
-  
-  console.log(stateLangDays[lang]); 
-} else {
-  console.log('Язык может быть только ru или en');
+
+  return newStr;
 }
 
-
-// 2
-const namePerson = 'Артём';
-
-const result = (namePerson === 'Артём') ? 'директор' :
-  (namePerson === 'Максим') ? 'преподаватель' :
-  'студент';
-console.log(result);
+// тесты
+deleteSpaces(' hello ');
+console.log(deleteSpaces(' hello '));
+console.log(deleteSpaces('  heeeeeeeeelllllllllllooooooouuuuuuu '));
 
 
 
