dd1f1b30e5d9640d52f949396a1368bacb65e469
Igor Trifonov <Ha1lowe3n@yandex.ru>
2020-10-15 04:29:18 +0800

    feat(script.js): new feat

--- script.js
+++ script.js
@@ -1,28 +1,42 @@
 'use strict';
 
-const deleteSpaces = (str) => {
-  let res = 'Вы не передали строку';
+let arr = ['87', '354', '7889', '26756858', '444', '87688', '9879677'];
 
-  const isNumber = function(n) {
-    return !isNaN(parseFloat(n) && isFinite(n));
-  };
+// 1
+for (let i = 0; i < arr.length; i++) {
+  if (arr[i].startsWith('2') || arr[i].startsWith('4')) {
+      console.log(arr[i]);
+  }
+}
 
-  if (typeof str === 'string' && isNumber(str) === false) {
-    const newStr = str.trim();
-    if (newStr.length > 30) {
-      res = newStr.substr(0, 30) + '...';
-    } else {
-      res = newStr;
+// 2 и 3
+const n = 100;
+
+numberLabel:
+for (let i = 2; i <= n; i++) {
+
+  for (let j = 2; j < i; j++) {
+    if (i % j === 0) {
+      continue numberLabel;
     }
   }
 
-  return res;
-};
+  console.log(i);
+  // ниже сделал с циклом, если нельзя просто взять и написать вот так
+  console.log(`Делители числа ${i}: 1 и ${i}`);
 
-// тесты
-console.log(deleteSpaces(123));
-console.log(deleteSpaces(' hello '));
-console.log(deleteSpaces('  dfdhfhfjghjghjghjhghfgjfjfgjfjfjgfhffhgjfg '));
+  // for (let y = i; y === i; y++) {
+  //   console.log(`Делители числа ${y}: 1 и ${i}`);
+  // }
+  
+}
+
+
+
+
+
+
+
 
 
 
