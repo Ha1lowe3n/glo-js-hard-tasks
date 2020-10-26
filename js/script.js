3d1a62a838224474ee2b8654ec4801545f7db066
Igor Trifonov <Ha1lowe3n@yandex.ru>
2020-10-26 20:39:58 +0800

    fix(script.js): delete useless file

--- js/script.js
+++ js/script.js
@@ -2,11 +2,9 @@
 
 const btnRegister = document.querySelector('#registerUser'),
       btnAuto = document.querySelector('#login'),
-      list = document.querySelector('#list');
-
-const isString = function(n) {
-  return n.trim() === '' || n === null;
-};
+      list = document.querySelector('#list'),
+      username = document.querySelector('#username');
+      
 
 const dateOptions = {
   year: 'numeric',
@@ -30,49 +28,73 @@ const addToStorage = () => {
   localStorage.setItem('appData', JSON.stringify(appData));
 };
 
-const start = () => {
-  let nameStr = prompt('Введите через пробел Имя и Фамилию пользователя');
-  console.log(nameStr.split(' ').length - 1);
-
-  if (!nameRegister.test(nameStr) || nameStr.split(' ').length - 1 > 1) {
-    alert('Ошибка ввода данных!');
-    return;
-  } 
-
-  let loginVar = prompt('Введите логин');
-
-  if (loginVar === null) {
-    return;
-  }
-
-  let passwordVar = prompt('Введите пароль');
-
-  const render = () => {
-    list.textContent = '';
+const render = () => {
+  list.textContent = '';
   
-    appData.forEach(function(item, i) {
-      const li = document.createElement('li');
+  appData.forEach(function(item, i) {
+    const li = document.createElement('li');
+  
+    li.innerHTML = '<span>' + 'Имя: ' + item.name + ', фамилия: ' + item.lastName + ', зарегистрирован: ' + item.regDate + '</span>' +
+      '<button class="close"></button>';
+    list.append(li); 
+
+    const btnClose = document.querySelector('.close');
     
-      li.innerHTML = '<span>' + 'Имя: ' + item.name + ', фамилия: ' + item.lastName + ', зарегистривован: ' + item.regDate + '</span>';
-      list.append(li);   
+    btnClose.addEventListener('click', () => {
+      appData.splice(i, 1);
+      localStorage.removeItem(appData[i]);
+      render();
+      addToStorage();
     });
-  }; 
+  });
 
-  const arr = nameStr.split(' ');
+  const start = () => {
+    let nameStr = prompt('Введите через пробел Имя и Фамилию пользователя');
+  
+    if (!nameRegister.test(nameStr) || nameStr.split(' ').length - 1 > 1) {
+      alert('Ошибка ввода данных!');
+      return;
+    } 
 
-  const newText = {
-    name: arr[0],
-    lastName: arr[1], 
-    login: loginVar,
-    password: passwordVar,
-    regDate: toDay
+    let loginVar = prompt('Введите логин');
+
+    if (loginVar === null) {
+      return;
+    }
+
+    let passwordVar = prompt('Введите пароль');
+
+    const arr = nameStr.split(' ');
+
+    const newText = {
+      name: arr[0],
+      lastName: arr[1], 
+      login: loginVar,
+      password: passwordVar,
+      regDate: toDay
+    };
+
+    appData.push(newText);
+    render();
+    addToStorage();
+    btnRegister.removeEventListener('click', start);
   };
 
-  appData.push(newText);
-  render();
-  addToStorage();
+  btnRegister.addEventListener('click', start); 
 };
 
-btnRegister.addEventListener('click', start);
+const authorization = () => {
+  let loginAuto = prompt('Введите логин');
+  let pasAuto = prompt('Введите пароль');
+
+  appData.forEach((item) => {
+    if (item.login === loginAuto && item.password === pasAuto) {
+      username.textContent = loginAuto;
+    }
+  });
+};
+
+btnAuto.addEventListener('click', authorization);
+
 render();
 
