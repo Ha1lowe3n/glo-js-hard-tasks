57d45d4eecaaab1917583165b4547b9aaf254c36
Igor Trifonov <Ha1lowe3n@yandex.ru>
2020-11-03 03:49:43 +0800

    feat(script.js): debounce wrapper

--- script.js
+++ script.js
@@ -1,36 +1,20 @@
 'use strict';
 
-const btn = document.querySelector('[data-start]'),
-      elem = document.querySelector('.box'),
-      btnClose = document.querySelector('[data-close]');
+const text = document.querySelector('#text');
 
-let pos = 0, 
-    animId,     
-    start = 0; 
-
-const myAnimation = () => {     
-  if (pos < 300 && start) {  
-    pos++;         
-    elem.style.top = pos + "px";         
-    elem.style.left = pos + 'px';         
-    animId = requestAnimationFrame(myAnimation);     
-  }  
+const debounce = (fn, ms) => {
+  let timeout;
+  return function () {
+    const fnCall = () => { fn.apply(this, arguments); };
+    clearTimeout(timeout);
+    timeout = setTimeout(fnCall, ms);
+  };
 };
+  
+let onChange = (e) => {
+  text.textContent = e.target.value;
+};
+  
+onChange = debounce(onChange, 3000);
 
-btn.addEventListener('click', () => {     
-  start = (start + 1) % 2;     
-  if (start) {         
-    animId = requestAnimationFrame(myAnimation);     
-  } else {         
-    cancelAnimationFrame(animId);     
-  } 
-});
-
-btnClose.addEventListener('click', () => {
-  start = 0;
-  pos = 0;
-  elem.style.top = pos + "px";         
-  elem.style.left = pos + 'px'; 
-});
-
-
+document.getElementById('search').addEventListener('input', onChange);
